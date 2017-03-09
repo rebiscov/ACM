d9f25b62b822c41f27de5dc12fa3ad4a48d1c449
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-09 15:54:22 +0100

    Finish almost prime numbers exercice, need to fix some problems with type

--- S5/10539-vrebisco.c
+++ S5/10539-vrebisco.c
@@ -4,18 +4,33 @@
 #define SIZE 1000000
 
 int tab[SIZE];
+int *primes = NULL;
+int nb_primes = 0;
 
-float log_b(float x, float b){ /* It computes logarithm in base b */
-  return log(x)/log(b);
+long double log_b(long double x, long double b){ /* It computes logarithm in base b */
+  return (long double)(logl(x)/logl(b));
 }
 
-void affiche(void){
+
+void count_prime(void){
   int i;
-  
-  tab[0] = 1; tab[1] = 1;
-  for (i = 2; i < SIZE; i++)
+  for (i = 0; i < SIZE; i++){
     if (tab[i] == 0)
-      printf("%d est premier\n", i);
+      nb_primes++;
+  }
+}
+
+void tab_primes(void){
+  int i, j = 0;
+
+  count_prime();
+  primes = malloc(sizeof(int)*nb_primes);
+  for (i = 0; i < SIZE; i++){
+    if (tab[i] == 0){
+      primes[j] = i;
+      j++;
+    }
+  }
 }
 
 int find_next(int i){
@@ -28,24 +43,54 @@ int find_next(int i){
     return j;
 }
 
-void crible(void){
+void crible(void){ /* Classical algorithm to find prime numbers */
   int i = 2, j;
   while (i*i < SIZE){
-    for (j = 2*i; j < SIZE; j +=i){
+    for (j = 2*i; j < SIZE; j += i){
       tab[j] = 1;
     }
     i = find_next(i);
   }
 }
 
+long int count_p(int p, long int low, long int high){ /*count the number of almost prime numbers with divisor p between low and high */
+  long int m = log_b((int)low, p), M = log_b((int)high, p);
+
+  if (M - m <= 0){
+    return 0;
+  }
+  else
+    return M - m - 1;
+}
+
+long int count(long int low, long int high){
+  int i;
+  long int sum = 0;
+  for (i = 0; i < sqrt(high)+1; i++){
+    sum += count_p(primes[i], low, high);
+  }
+  
+  return sum;
+}
+
 int main(void){
-  int i, ;
-  for (i = 0; i < SIZE; i++){
+  int i, N;
+  long int low, high;
+
+  tab[0] = 1; tab[1] = 1;
+  for (i = 2; i < SIZE; i++){
     tab[i] = 0;
   }
   crible(); /* We now have a boolean array wich contains all the prime numbers between 2 and SIZE */
-  
-  
+  tab_primes(); /* Now we have an array with the prime numbers */
+
+
+  scanf("%d", &N);
+
+  for (i = 0; i < N; i++){
+    scanf("%d %d", &low, &high);
+    printf("%d\n", count(low, high));
+  }
 
   return 0;
 }
