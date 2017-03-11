9fb2ee9a3d551b3d93c1f288942390334ffa9689
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-11 14:00:06 +0100

    108 finished

--- S5/108-vrebisco.c
+++ S5/108-vrebisco.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-long int opt[100][100][100][100]; /* We're gonna do a dynamic programm, and here also called "brut-eforce with a fancy name" */
+int opt[100][100][100][100]; /* We're gonna do a dynamic programm, and here also called "brute-force with a fancy name" */
+/* opt[i][k][j][l] contains the sum of the rectangle with the top left corner of cordinate (i,k) and bottom right corner (j,l) */
 int N;
 
 void affiche(void){
@@ -16,41 +17,44 @@ void affiche(void){
 
 int main(void){
   int i, j, k, l;
-  long int max = 0;
+  int max;
   
-  scanf("%d", &N);
-  
-  for (i = 0; i < N; i++)
-    for (j = 0; j < N; j++){
-      scanf("%d", &opt[i][j][i][j]);
-      if (max > opt[i][j][i][j])
-	max = opt[i][j][i][j];
-    }
-
-  affiche();
-  
-  for (i = 0; i < N; i++){
-    for (k = 0; k < N; k++){
-      for (l = k + 1; l < N; l++){
-	opt[i][k][i][l] = opt[i][k][i][l-1]+opt[i][l][i][l];
-	if (max > opt[i][k][i][l])
-	  max = opt[i][k][i][l];
+  while (scanf("%d", &N) != EOF){
+    max = 0;
+    
+    for (i = 0; i < N; i++)
+      for (j = 0; j < N; j++){
+	scanf("%d", &opt[i][j][i][j]);
+	if (i == 0 && j == 0)
+	  max = opt[i][j][i][j];
+	else if (max < opt[i][j][i][j])
+	  max = opt[i][j][i][j];
       }
-    }
-  }
 
-  for (i = 0; i < N; i++){
-    for (j = i + 1; j < N; j++){
+  
+    for (i = 0; i < N; i++){
       for (k = 0; k < N; k++){
 	for (l = k + 1; l < N; l++){
-	  opt[i][k][j][l] = opt[i][k][j-1][l] + opt[j][k][j][l];
-	  if (max > opt[i][k][j][l])
-	    max = opt[i][k][j][l];
-	}	
+	  opt[i][k][i][l] = opt[i][k][i][l-1]+opt[i][l][i][l];
+	  if (max < opt[i][k][i][l])
+	    max = opt[i][k][i][l];
+	}
       }
     }
+    
+    for (i = 0; i < N; i++){
+      for (j = i + 1; j < N; j++){
+	for (k = 0; k < N; k++){
+	  for (l = k + 1; l < N; l++){
+	    opt[i][k][j][l] = opt[i][k][j-1][l] + opt[j][k][j][l];
+	    if (max < opt[i][k][j][l])
+	      max = opt[i][k][j][l];
+	  }	
+	}
+      }
+    }
+    printf("%d\n", max);
   }
-  printf("%d\n", opt[1][0][3][1]);
   
   return 0;
 }
