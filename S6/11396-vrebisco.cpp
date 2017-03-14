075fab6899cb4909d4fd1be75e4070cfcb09e512
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-14 19:05:36 +0100

    Doing shit

--- S6/11396-vrebisco.cpp
+++ S6/11396-vrebisco.cpp
@@ -26,19 +26,21 @@ int oppose(int colors){
     return BLANK;
 }
 
-int dfs(unsigned int n, unsigned int m, unsigned int u, int color){
+int dfs(unsigned int n, unsigned int u, int color){
   unsigned int i, v;
   int color_opp = oppose(color);
+  printf("pere %u\n", u);
   
   colors[u] = GREY;
   for (i = 0; i < graph[u].size(); i++){
+    printf("fils %d\n", i);
     v = graph[u][i];
     if (colors_aux[v] == color)
       return 1;
     else
       colors_aux[v] = color_opp;
     if (colors[v] == WHITE){
-      dfs(n, m, v, oppose(color));
+      dfs(n, v, oppose(color));
     }
   }
 
@@ -46,12 +48,12 @@ int dfs(unsigned int n, unsigned int m, unsigned int u, int color){
 }
 
 int main(void){
-  unsigned int n, m, i, u, v, ret;
+  unsigned int n, i, j, u, v, ret;
 
 
   while(true){
-    scanf("%u %u", &n, &m);
-    if (n == 0 && m == 0)
+    scanf("%u", &n);
+    if (n == 0)
       break;
 
     for (i = 0; i < 100; i++){
@@ -60,16 +62,27 @@ int main(void){
       colors_aux[i] = BLANK;
     }
 
-    for (i = 0; i < m; i++){
+    do{
       scanf("%u %u", &u, &v);
       u--; v--;
       graph[u].push_back(v);
-    }
+      graph[v].push_back(u);
+    } while (u != 0 && v != 0);
 
-    dfs(n, m, 0, RED);    
+    dfs(n, 0, RED);    
     for (i = 1; i < n; i++){
+      printf("i = %d\n", i);
       if (colors[i] == WHITE){
-	ret = dfs(n, m, i, oppose(colors[graph[i][0]]));
+	colors_aux[i] = RED;
+	for (j = 0; j < graph[i].size(); j++){
+	  v = graph[i][j];
+	  if (colors_aux[v] != BLANK){
+	    colors_aux[i] = oppose(colors_aux[v]);
+	    break;
+	  }
+	}
+	ret = dfs(n, u, colors_aux[i]);
+	
 	if (ret == 1){
 	  break;
 	}
