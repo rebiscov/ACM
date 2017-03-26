6fcc5e09d2517b705cdd1ce33bf1d1f8e012ff62
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-03-26 23:56:53 +0200

    Finished

--- S7/10369-vrebisco.cpp
+++ S7/10369-vrebisco.cpp
@@ -29,7 +29,11 @@ int cmp_edge(const void *a, const void *b){
   return ((edge*)a)->d - ((edge*)b)->d;
 }
 
-int find(int x, element *P){ /* We find, and compress the structure */
+bool cmp_edge2(edge a, edge b){
+  return a.d < b.d;
+}
+
+int find(int x, std::vector<element> &P){ /* We find, and compress the structure */
   int y;
   
   if (P[x].x == x)
@@ -59,32 +63,31 @@ coor vertices[NB_VERTICES];
 std::vector<edge> edges;
 std::vector<element> Part;
 std::vector<edge> tree;
-bool tab[NB_VERTICES*NB_VERTICES];
 
 int main(void){ /* Kruskal algorithm... again */
-  unsigned int N, S, P, a, b, nb, i, j, k;
+  long int N, S, P, a, b, i, j, k;
   edge e;
   element p;
 
-  scanf("%u", &N);
+  scanf("%d", &N);
 
   for (i = 0; i < N; i++){
     edges.clear();
     Part.clear();
     
-    scanf("%u %u", &S, &P);
+    scanf("%d %d", &S, &P);
     
     for (j = 0; j < P; j++)
       scanf("%d %d", &vertices[j].x, &vertices[j].y);
 
     for (j = 0; j < P; j++){
       for (k = 0; k < j; k++){
-	e.u = j; e.v = k; e.d = pow((vertices[j].x - vertices[k].x), 2) + pow((vertices[j].y - vertices[k].y), 2);
+	e.u = j; e.v = k; e.d = (vertices[j].x - vertices[k].x)*(vertices[j].x - vertices[k].x) + (vertices[j].y - vertices[k].y)*(vertices[j].y - vertices[k].y);
 	edges.push_back(e);
       }
     }
 
-    qsort(&edges[0], edges.size(), sizeof(edge), cmp_edge);
+    std::sort(edges.begin(), edges.end(), cmp_edge2);
 
     for (j = 0; j < edges.size(); j++){
       p.x = j; p.nb = 1;
@@ -92,36 +95,20 @@ int main(void){ /* Kruskal algorithm... again */
     }
     tree.clear();
 
-    for (j = 0; j < edges.size(); j++){
-      a = find(edges[j].u, &Part[0]); b = find(edges[j].v, &Part[0]);
+    for (j = 0; j < edges.size(); j++){ /* Kruskal algorithm */
+      a = find(edges[j].u, Part); b = find(edges[j].v, Part);
       if (a != b){
 	tree.push_back(edges[j]);
 	unify(a, b, Part);
       }
     }
 
-    if (S == 0){
-      printf("%.2f\n", sqrt(tree[tree.size()-1].d));
-      continue;
-    }
-
-    /*int l = S, n = tree.size()-1;
-    nb = 0;
-    while (l >= 0){
-      e = tree[nb];
-      if (!tab[e.u]){
-	tab[e.u] = true;
-	l--;
-      }
-      if (!tab[e.v]){
-	tab[e.v] = true;
-	l--;
-      }
-      if (l >= 0)
-	nb++;
-	}*/
-
-    printf("%.2f\n", sqrt(tree[tree.size()-S].d));
+    if (S > tree.size())
+      printf("%.2f\n", 0.0);
+    else if (S == 0) /* This case should never happen */
+      printf("%.2f\n", sqrt((double)tree[tree.size()-1].d));
+    else
+      printf("%.2f\n", sqrt((double)tree[tree.size()-S].d));
   }
   
   return 0;
