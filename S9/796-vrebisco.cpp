89f3468035741796d4f3d1ae6c3a13c49ca2da27
Vincent RÉBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-04-09 20:57:21 +0200

    Advancing on exercise

--- S9/796-vrebisco.cpp
+++ S9/796-vrebisco.cpp
@@ -3,6 +3,15 @@
 #include <stack>
 #include <utility>
 
+void print_stack(std::stack<unsigned int> s){
+  printf("Stack: ");
+  while (!s.empty()){
+    printf("%u ", s.top());
+    s.pop();
+  }
+  printf("\n");
+}
+
 int main(void){
   unsigned int n, nb_n, u, v;
 
@@ -29,10 +38,12 @@ int main(void){
     for(unsigned int j = 0; j < n; j++){
       printf("j = %u\n", j);
       if (!seen[j]){
+	printf("push %u\n", j);
 	s.push(j);
 	
 	while (!s.empty()){
 	  unsigned int c_u = s.top();
+	  s.pop();
 	  
 	  if (seen[c_u]){
 	    s.pop();
@@ -41,13 +52,18 @@ int main(void){
 	  seen[c_u] = true;
 	  
 	  for (unsigned int k = 0; k < graph[c_u].size(); k++){
+	    print_stack(s);
 	    printf("  k = %u\n", k);
-	    if(!seen[graph[c_u][k]])
+	    printf("  c_u = %u\n", c_u);
+	    if(!seen[graph[c_u][k]]){
+	      printf("push parcours %u\n", graph[c_u][k]);
 	      s.push(graph[c_u][k]);
+	    }
 	    
 	    else if (!((graph[c_u][k] == last_e.first && c_u == last_e.second) || (graph[c_u][k] == last_e.second && last_e.first == c_u))){
 	      std::stack<unsigned int> t;
 	      unsigned int vert = graph[c_u][k];
+	      printf("search for %u\n", graph[c_u][k]);
 	      while (s.top() != graph[c_u][k]){
 		printf("    +1\n");
 		t.push(s.top());
@@ -57,7 +73,11 @@ int main(void){
 		vert = s.top();
 		printf("    etop\n");
 		s.pop();
+		printf("    end\n");
+		if (s.empty())
+		  printf("! EMPTY !\n");
 	      }
+	      printf("yo\n");
 	      while (!t.empty()){
 		printf("    -1\n");
 		s.push(t.top());
@@ -65,10 +85,9 @@ int main(void){
 	      }
 	    }
 	  }
-	  unsigned int a = s.top();
-	  s.pop();
-	  if (!s.empty() && !edges[a][s.top()])
-	    e.push_back(std::make_pair (a, s.top()));
+	  if (!s.empty() && !edges[c_u][s.top()])
+	    e.push_back(std::make_pair (c_u, s.top()));
+	  last_e.first = c_u; last_e.second = s.top();
 	  
 	}
       }
